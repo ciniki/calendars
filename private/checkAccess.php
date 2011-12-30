e6883098b62814e53d105738b7c1af3c58dfd4fb
Andrew Rivett <veggiefrog@gmail.com>
2011-12-29 22:40:40 -0500

    Setup new permissions system for package support

--- private/checkAccess.php
+++ private/checkAccess.php
@@ -13,29 +13,29 @@
 // =======
 //
 function ciniki_calendars_checkAccess($ciniki, $business_id, $method) {
+    //  
+    // Check if the module is turned on for the business
+    // Check the business is active
+    // Get the ruleset for this module
+    //  
+    $strsql = "SELECT ruleset "
+        . ", CONCAT_WS('.', ciniki_business_modules.package, ciniki_business_modules.module) AS module_id "
+        . "FROM ciniki_businesses, ciniki_business_modules "
+        . "WHERE ciniki_businesses.id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+        . "AND ciniki_businesses.status = 1 "                                                       // Business is active
+        . "AND ciniki_businesses.id = ciniki_business_modules.business_id "
+        . "AND ciniki_business_modules.status = 1 "                                                     // Business is active
+        . ""; 
+    require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbHashIDQuery.php');
+    $rc = ciniki_core_dbHashIDQuery($ciniki, $strsql, 'businesses', 'modules', 'module_id');
+    if( $rc['stat'] != 'ok' ) { 
+        return $rc;
+    }   
+    if( !isset($rc['modules']) || !isset($rc['modules']['ciniki.calendars']) ) { 
+        return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'495', 'msg'=>'Access denied.'));
+    }   
+    $modules = $rc['modules'];
 
-	//
-	// Check if the module is turned on for the business
-	// Check the business is active
-	// Get the ruleset for this module
-	//
-	$strsql = "SELECT ruleset "
-		. ", CONCAT_WS('.', ciniki_business_modules.package, ciniki_business_modules.module) AS module_id "
-		. "FROM ciniki_businesses, ciniki_business_modules "
-		. "WHERE ciniki_businesses.id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
-		. "AND ciniki_businesses.status = 1 "														// Business is active
-		. "AND ciniki_businesses.id = ciniki_business_modules.business_id "
-		. "AND ciniki_business_modules.status = 1 "														// Business is active
-		. "";
-	require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbHashIDQuery.php');
-	$rc = ciniki_core_dbHashIDQuery($ciniki, $strsql, 'businesses', 'modules', 'module_id');
-	if( $rc['stat'] != 'ok' ) {
-		return $rc;
-	}
-	if( !isset($rc['modules']) || !isset($rc['modules']['ciniki.calendars']) ) {
-		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'495', 'msg'=>'Access denied.'));
-	}
-	$modules = $rc['modules'];
 
 	//
 	// Sysadmins are allowed full access
@@ -50,7 +50,8 @@ function ciniki_calendars_checkAccess($ciniki, $business_id, $method) {
 	$strsql = "SELECT business_id, user_id FROM ciniki_business_users "
 		. "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
 		. "AND user_id = '" . ciniki_core_dbQuote($ciniki, $ciniki['session']['user']['id']) . "' "
-		. "AND (groups&0x03) > 0 "
+		. "AND package = 'ciniki' "
+		. "AND (permission_group = 'owners' OR permission_group = 'employees') "
 		. "";
 	require_once($ciniki['config']['core']['modules_dir'] . '/core/private/dbHashQuery.php');
 	$rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'businesses', 'user');
@@ -58,18 +59,16 @@ function ciniki_calendars_checkAccess($ciniki, $business_id, $method) {
 		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'496', 'msg'=>'Access denied.'));
 	}
 	//
-	// Double check business_id and user_id match, for single row returned.
+	// If the user has permission, return ok
 	//
-	if( isset($rc['user']) && isset($rc['user']['business_id']) 
-		&& $rc['user']['business_id'] == $business_id 
-		&& $rc['user']['user_id'] = $ciniki['session']['user']['id'] ) {
-		// Access Granted!
+	if( isset($rc['rows']) && isset($rc['rows'][0]) 
+		&& $rc['rows'][0]['user_id'] > 0 && $rc['rows'][0]['user_id'] == $ciniki['session']['user']['id'] ) {
 		return array('stat'=>'ok', 'modules'=>$modules);
 	}
 
 	//
 	// By default, fail
 	//
-	return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'510', 'msg'=>'Access denied.'));
+	return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'495', 'msg'=>'Access denied.'));
 }
 ?>
