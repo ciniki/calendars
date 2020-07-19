79f0f0daaede21da4b4867c1a91a89e6a0970022
Andrew Rivett <veggiefrog@gmail.com>
2020-07-18 23:02:12 -0400

    Updated to json format for main menu caledar search

--- hooks/uiSettings.php
+++ hooks/uiSettings.php
@@ -42,39 +42,39 @@ function ciniki_calendars_hooks_uiSettings($ciniki, $tnid, $args) {
                 'cellClasses'=>array('multiline slice_0', 'schedule_appointment'),
                 'cellColours'=>array(
                     '0'=>'\'\'',
-                    '1'=>'if( d.appointment != null && d.appointment.colour != null && d.appointment.colour != \'\' ) {'
-                            . 'd.appointment.colour;'
+                    '1'=>'if( d.colour != null && d.colour != \'\' ) {'
+                            . 'd.colour;'
                         . '} else {'
                             . '\'#77ddff\';'
                         . '}',
                     ),
                 'cellValues'=>array(
-                    '0'=>'if( d.appointment.start_ts == 0 ) {'
+                    '0'=>'if( d.start_ts == 0 ) {'
                             . '"unschedule";'
                         . '} else if( d.appointment_allday == "yes" ) {'
-                            . 'd.appointment.start_date.split(/ [0-]+:/)[0];'
+                            . 'd.start_date.split(/ [0-]+:/)[0];'
                         . '} else {'
-                            . '\'<span class="maintext">\' + d.appointment.start_date.split(/ [0-9]+:/)[0] + \'</span><span class="subtext">\' + d.appointment.start_date.split(/, [0-9][0-9][0-9][0-9] /)[1] + \'</span>\''
+                            . '\'<span class="maintext">\' + d.start_date.split(/ [0-9]+:/)[0] + \'</span><span class="subtext">\' + d.start_date.split(/, [0-9][0-9][0-9][0-9] /)[1] + \'</span>\''
                         . '}',
                     '1'=>'var t="";'
-                        . 'if( d.appointment.secondary_colour != null && d.appointment.secondary_colour != \'\' ) {'
-                            . 't += \'<span class="colourswatch" style="background-color:\' + d.appointment.secondary_colour + \'">\';'
-                            . 'if( d.appointment.secondary_colour_text != null && d.appointment.secondary_colour_text != \'\' ) { '
-                                . 't += d.appointment.secondary_colour_text; '
+                        . 'if( d.secondary_colour != null && d.secondary_colour != \'\' ) {'
+                            . 't += \'<span class="colourswatch" style="background-color:\' + d.secondary_colour + \'">\';'
+                            . 'if( d.secondary_colour_text != null && d.secondary_colour_text != \'\' ) { '
+                                . 't += d.secondary_colour_text; '
                             . '} else {'
                                 . 't += \'&nbsp;\';'
                             . '} '
                             . 't += \'</span> \''
                         . '} '
-                        . 't += d.appointment.subject;'
-                        . 'if( d.appointment.secondary_text != null && d.appointment.secondary_text != \'\' ) {'
-                            . 't += \' <span class="secondary">\' + d.appointment.secondary_text + \'</span>\';'
+                        . 't += d.subject;'
+                        . 'if( d.secondary_text != null && d.secondary_text != \'\' ) {'
+                            . 't += \' <span class="secondary">\' + d.secondary_text + \'</span>\';'
                         . '} '
                         . 't;',
                     ),
 //                'cellEdits'=>array(
-//                    '0'=>array('method'=>'ciniki.calendars.main', 'args'=>array('date'=>'d.appointment.date')),
-//                    '1'=>array('method'=>'d.app;', 'args'=>array('appointment_id'=>'d.appointment.id')),
+//                    '0'=>array('method'=>'ciniki.calendars.main', 'args'=>array('date'=>'d.date')),
+//                    '1'=>array('method'=>'d.app;', 'args'=>array('appointment_id'=>'d.id')),
 //                    ),
                 'noData'=>'No appointments found',
 //                'edit'=>array('method'=>'ciniki.calendars.main', 'args'=>array('order_id'=>'d.order.id;')),
